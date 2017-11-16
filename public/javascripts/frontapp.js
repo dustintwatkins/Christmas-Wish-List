19ea4c1faac586e8ac9d11a15a78098d2775ec14
dustintwatkins <dustintwatkins@gmail.com>
2017-11-16 00:48:28 +0000

    first commit

---
+++ public/javascripts/frontapp.js
@@ -0,0 +1,39 @@
+var app = window.angular.module('app', [])
+
+app.factory('giftFetch', giftFetch)
+app.controller('mainCtrl', mainCtrl)
+
+function giftFetch($http){
+	var API_ROOT = 'gift'
+	return{
+		get:function(){
+			return $http
+			.get(API_ROOT)
+			.then(function (resp) {
+				return resp.data
+			})
+		},
+		post: function (formData){
+			return $http
+			.post(API_ROOT,formData)
+			.then(function (resp){
+				console.log("Post Worked");
+			})
+		}
+	}
+}
+
+function mainCtrl ($scope, giftFetch){
+	$scope.gifts = []
+
+	giftFetch.get()
+		.then(function (data){
+			$scope.gifts = data
+		})
+		$scope.addGift = function(){
+			var formData = {Item:$scope.Item,Image:$scope.Url};
+			console.log(formData);
+			giftFetch.post(formData);
+			$scope.gifts.push(formData);
+		}
+}
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+//var request = require('request');
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+var dbUrl = 'mongodb://localhost/gifts';
+
+var collection = 
+
+MongoClient.connect(dbUrl, function(err,db){
+	if(err){console.log('unable to connect to the MongoDB server. Error:', err);}
+	else{
+		console.log('Connection established to', dbUrl);
+	}	
+});
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/gift', function(req, res){
+  console.log("In Gift Post");
+  console.log(req.body);
+  gift.push(req.body);
+  res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+router.get('/gift', function(req,res){
+	console.log("in gift route");
+	res.send(gift);
+});
+
+var gift = [];
+
+
+module.exports = router;
